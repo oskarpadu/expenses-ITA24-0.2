00e3c008e1ce6005334bf460daa717a4ccdd4705
oskar <oskarpadu31@gmail.com>
2026-01-09 11:43:35 +0200

    jaotamine mitmeks komponendiks

--- 2expens/src/components/Expenseitem.jsx
+++ 2expens/src/components/Expenseitem.jsx
@@ -1,16 +1,17 @@
+import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
 
 const ExpenseItem = (props) => {
     
-    console.log(props);
-    console.log(props.date);
+    console.log(props.data.date);
     
     return (
         <div className="expense-item">
-            <div>{props.date.toString()}</div>
+            <ExpenseDate date={props.data.date} />
+            <div>{props.data.date.toString()}</div>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.price}</div>
+                <h2>{props.data.title}</h2>
+                <div className="expense-item__price">${props.data.price}</div>
             </div>
         </div>
     );
