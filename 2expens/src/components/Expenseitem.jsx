d800d9da8ead0c060f687f59931711e81566b518
oskar <oskarpadu31@gmail.com>
2026-01-15 14:25:25 +0200

    Komponentide organiseerimine

--- 2expens/src/components/Expenseitem.jsx
+++ 2expens/src/components/Expenseitem.jsx
@@ -1,5 +1,6 @@
 import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
+import Card from '../ui/Card';
 
 const ExpenseItem = (props) => {
     
@@ -7,14 +8,14 @@ const ExpenseItem = (props) => {
     
     return (
 
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.data.date} />
             <div>{props.data.date.toString()}</div>
             <div className="expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">${props.data.price}</div>
             </div>
-        </div>
+        </Card>
     );
 };
 
--- 2expens/src/components/Expenses.jsx
+++ 2expens/src/components/Expenses.jsx
@@ -1,5 +1,5 @@
 import './Expenses.css';
-import Card from './Card';
+import Card from '../ui/Card';
 
 const Expenses = (props) => {
   return (
