c5ad3bd050923b16625811edb199a52692cb634b
oskar <oskarpadu31@gmail.com>
2026-01-22 13:12:40 +0200

    Button ja Sethook

--- 2expens/src/components/Expenses/Expenseitem.jsx
+++ 2expens/src/components/Expenses/Expenseitem.jsx
@@ -3,6 +3,13 @@ import '../Expenses/Expenseitem.css';
 import Card from '../ui/Card.jsx';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expensedata.title);
+
+    const clickHandler = () => {
+        console.log('Clicked!');
+        setTitle(`Updated by Click ${title}`);
+        console.log(title);
+    };
     
     console.log(props.data.date);
     
@@ -15,6 +22,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">${props.data.price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>
         </Card>
     );
 };
