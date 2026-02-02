bd8a1901adfb462a01cc307780e91e38023a702e
oskar <oskarpadu31@gmail.com>
2026-02-02 09:59:12 +0200

    Fragment, Portals ja useRef

--- 2expens/src/components/NewExpense/ExpenseFrom.jsx
+++ 2expens/src/components/NewExpense/ExpenseFrom.jsx
@@ -1,67 +1,68 @@
-import { useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import './ExpenseForm.css';
+import Error from '../ui/Error.jsx';
 
 const ExpenseForm = (props) => {
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredPrice: '',
-        enteredDate: ''
-    });
+    const [error, setError] = useState(null);
+    console.log(error);
 
-    const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        });
-    };
+    const titleInputRef = useRef();
+    const priceInputRef = useRef();
+    const dateInputRef = useRef();
 
-    const priceChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredPrice: event.target.value
-        });
-    };
-    
-    const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        });
+    const errorHandler = () => {
+        setError(null);
     };
 
     const submitHandler = (event) => {
-        event.preventDefault();
-        const expenseData = {
-            title: userInput.enteredTitle,
-            amount: +userInput.enteredPrice,
-            date: new Date(userInput.enteredDate)
-        };
-        props.onSaveExpenseData(expenseData);
-        setUserInput({
-            enteredTitle: '',
-            enteredPrice: '',
-            enteredDate: ''
-        });
+        const enteredTitle = titleInputRef.current.value;
+        const enteredPrice = priceInputRef.current.value;
+        const enteredDate = dateInputRef.current.value;
 
-        setUserInput({ enteredTitle: '', enteredPrice: '', enteredDate: '' });
+        event.preventDefault();
+
+        if (enteredTitle.trim().length === 0 || enteredPrice.trim().length === 0 || enteredDate.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid title, amount, and date (non-empty values).'
+            });
+            return;
+        }
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: +enteredPrice,
+            date: new Date(enteredDate)
+        };
+
+
+        props.onSaveExpenseData(expenseData);
+        props.onCancel();
+        titleInputRef.current.value = '';
+        priceInputRef.current.value = '';
+        dateInputRef.current.value = '';
+
     };
 
 
     return (
+        <Fragment>
+            {error && (<Error title={error.title} message={error.message} onConfirm={errorHandler} />)}
+        <div>
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} value={userInput.enteredTitle}/>
+                    <input type='text' id="title" ref={titleInputRef}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={priceChangeHandler} value={userInput.enteredPrice}/>
+                    <input type='number' min='0.01' step='0.01' id="price" ref={priceInputRef}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2000-01-01' max='2028-12-31' onChange={dateChangeHandler} value={userInput.enteredDate}/>
+                    <input type='date' min='2000-01-01' max='2028-12-31' id="date" ref={dateInputRef}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
@@ -69,6 +70,8 @@ const ExpenseForm = (props) => {
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
+        </div>
+    </Fragment>
     );
 };
 
---
+++ 2expens/src/components/ui/Error.jsx
@@ -0,0 +1,47 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import './Error.css';
+
+import Card from './Card.jsx';
+import Button from './Button.jsx';
+
+const Backdrop = (props) => {
+    return <div className="backdrop" onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className="modal">
+            <header className="header">
+                <h2>{props.title}</h2>
+            </header>
+            <div className="content">
+                <p>{props.message}</p>
+            </div>
+            <footer className="footer">
+                <Button onClick={props.onClose}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
+const Error = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </Fragment>
+    );
+};
+
+export default Error;
